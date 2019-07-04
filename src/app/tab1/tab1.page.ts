6da042c65c9f34de327ff74afc310971252e8282
Payel <payel.karmakar@pwc.com>
2019-07-04 18:02:14 +0530

    Photo-gallery - with 4 different views

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,25 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  messages: Array<string>;
+
+  constructor(public navCtrl: NavController) {
+    this.messages = [
+      "Trying some ways, to check if that works!",
+      "Your limitation—it's only your imagination.",
+      "Great things never come from comfort zones.",
+      "Let's try to focus, let's have a goal! Be it for a near future, or far - however it suits you..",
+      "Success doesn't just find you. ...",
+      "Keep on the fire within you, let others burn..",
+      "The harder you work for something, the greater you'll feel when you achieve it.",
+      "Wake up with determination. Go to bed with satisfaction.",
+      "Don’t wait for opportunity. Create it.",
+      "The key to success is to focus on goals, not obstacles.",
+      "Dream it. Believe it. Build it.",
+      "Don’t stop when you’re tired. Stop when you’re done.",
+      "Be transparent, but do not let others see your soul!",
+      "The harder you work for something, the greater you’ll feel when you achieve it."
+    ]
+  }
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,9 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  pageTitle: string;
+  constructor(public navCtrl: NavController) {
+    this.pageTitle = 'Pinterest';
+  }
 
 }
